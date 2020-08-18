aa2000772fef169656bd5d0e44dd0a32ce05b475
bartvanhoey <bartvanhoey@hotmail.com>
2020-08-18 16:26:26 +0200

    languages

---
+++ Models/LanguageSkill.cs
@@ -0,0 +1,11 @@
+namespace bartvanhoey.Models
+{
+    public class LanguageSkill
+    {
+        public string PathToFlag { get; set; }
+        public string AltTextFlag { get; set; }
+        public string Speaking { get; set; }
+        public string Writing { get; set; }
+        public string Reading { get; set; }
+    }
+}
---
+++ Pages/Languages/MyLanguages.razor.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using bartvanhoey.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace bartvanhoey.Pages.Languages
+{
+  public class MyLanguagesBase : ComponentBase
+  {
+    protected List<LanguageSkill> Languages = new List<LanguageSkill>();
+
+    protected override void OnInitialized()
+    {
+      Languages.Add(new LanguageSkill
+      {
+        PathToFlag = @"\img\dutch.png",
+        AltTextFlag = "Dutch",
+        Speaking = "Native Speaker",
+        Writing = "Native Speaker",
+        Reading = "Native Speaker"
+      });
+
+      Languages.Add(new LanguageSkill
+      {
+        PathToFlag = @"\img\french.png",
+        AltTextFlag = "French",
+        Speaking = "Very good command",
+        Writing = "Excellent Command",
+        Reading = "Excellent Command"
+      });
+
+      Languages.Add(new LanguageSkill
+      {
+        PathToFlag = @"\img\english.png",
+        AltTextFlag = "English",
+        Speaking = "Very good command",
+        Writing = "Excellent command",
+        Reading = "Excellent command"
+      });
+
+      Languages.Add(new LanguageSkill
+      {
+        PathToFlag = @"\img\german.png",
+        AltTextFlag = "German",
+        Speaking = "Good command",
+        Writing = "Very good command",
+        Reading = "very good command"
+      });
+
+      Languages.Add(new LanguageSkill
+      {
+        PathToFlag = @"\img\spanish.png",
+        AltTextFlag = "Spanish",
+        Speaking = "Good command",
+        Writing = "Very good command",
+        Reading = "very good command"
+      });
+    }
+  }
+}
